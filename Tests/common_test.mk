27bdae627c770e977e0b08c0555288d87703640e
AngelPerezM <groverperezm-2000@hotmail.com>
2021-02-21 22:42:28 +0100

    common_test includes Makevars.mk

--- Tests/common_test.mk
+++ Tests/common_test.mk
@@ -1,45 +1,11 @@
-MV		:=mv -f
-RM		:=rm -f
-SED		:=sed
-EXT_SRC 	:=cpp
-AR		:=ar
-ARFLAGS		:=-crs
-
 # Definition of paths:
+MY_DIR		:=$(patsubst %/,%,$(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
 CWD 		 =$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-DIR_ROOT	?=../..
-DIR_INC		:=$(DIR_ROOT)/include
 DIR_SRC		:=$(CWD)
-DIR_OBJ		:=$(CWD)/build
-DIR_BIN		:=$(CWD)/binaries
+DIR_TEST	:=$(CWD)
+DIR_LIB		:=$(CWD)
 
-# Files:
-SOURCES		:=$(wildcard $(DIR_SRC)/*.$(EXT_SRC))
-OBJECTS		:=$(patsubst $(DIR_SRC)/%.$(EXT_SRC),$(DIR_OBJ)/%.o,$(SOURCES))
-DEPENDENCIES	:=$(OBJECTS:.o=.d)
-$(info [INFO] Sourecs: $(SOURCES))
-$(info [INFO] Objects: $(OBJECTS))
-$(info [INFO] Dependencies: $(DEPENDENCIES))
-
-# Compiler variables:
-CXX =g++
-CXXFLAGS =-std=c++11
-DEPFLAGS =-MMD
-
-COMON_FLAGS := -Wall -Wextra -Werror -Wformat=2 -pipe \
-		-Wlogical-op -Wduplicated-cond \
-		-Wduplicated-branches $(addprefix -I ,$(DIR_INC)) \
-		-L$(DIR_ROOT)/lib
-
-# Default option is debug mode.
-BUILD_DEBUG ?= yes
-ifeq ($(BUILD_DEBUG), yes)  # -- Debug mode?
-  COMON_FLAGS += -g -DDEBUG -O0
-  $(info [INFO] All files will be compiled in debug mode.)
-else
-  COMON_FLAGS += -Os # <--- for size optimization. Default by G++: O0.
-  $(info [INFO] All files will be compiled in release mode.)
-endif
+include $(MY_DIR)/../Makevars.mk
 
 ################################################################################
 
