651bc48d37346f02a33ef02392618ec181eece7d
taste <taste@taste10.homelan>
2021-06-18 18:05:33 +0200

    changes position of some scripts

---
+++ start_tasec_lab.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+bins="/home/taste/taste-workspace/b2space/TASTE-implementation/work/binaries"
+ip_rpi="192.168.5.2"
+ip_hp="192.168.5.3"
+
+cd ${bins}
+
+wait_for_key () {
+    echo $1
+    while [ true ] ; do
+        read -t 3 -n 1
+        if [ $? = 0 ] ; then
+            break
+        fi
+    done
+}
+
+# 1) RPi:
+###############################################################################
+if [ -z "$1"  ]; then
+    # empty
+    obsw_name="TASEC-LAB.exe"
+else
+    obsw_name="TASEC-LAB-NO-HTL.exe"
+fi
+
+# check if already executing and kill it if true:
+obsw_pid=$(ssh pi@${ip_rpi} "pidof ${obsw_name}")
+[ "${obsw_pid}" -ge "1" ] || ssh pi@${ip_rpi} 'kill ${obsw_pid}'
+
+obsw_pid=$(ssh pi@${ip_rpi} "pidof ${obsw_name}")
+if [[  "${obsw_pid}" -ge "1" ]]; then
+    # not empty
+    echo "[ERROR] OBSW was already executing and could not be stopped."
+    wait_for_key "Press any key to finish."
+    exit 1
+fi
+
+# start obsw:
+ssh pi@${ip_rpi} "~/${obsw_name} &"
+obsw_pid=$(ssh pi@${ip_rpi} "pidof ${obsw_name}")
+if [[ "${obsw_pid}" -lt "1" ]]; then
+    # emp   ty
+    echo "[ERROR] Could not start Raspberry Pi program."
+    wait_for_key "Press any key to finish."
+    exit 1
+else
+    echo "[INFO] Raspberry Pi program is executing."
+fi
+
+# 2) HP:
+###############################################################################
+./run_x86_partition_partition &> /dev/null &
+gnd_pid=$!
+if [[ "${gnd_pid}" -lt "1" ]]; then
+    # empty
+    echo "[ERROR] Could not start Graphical User Interface."
+    wait_for_key "Press any key to finish."
+    exit 1
+else
+    echo "[INFO] Graphical User Interface is executing."
+fi
+
+wait_for_key "Press any key to finish the execution."
+
+kill $obsw_pid
+pkill GUI
+
+exit 0
