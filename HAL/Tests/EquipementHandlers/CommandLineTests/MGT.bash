7aa773195abfb7fa71b502a8651653572b2f07cc
AngelPerezM <angel.perez.munoz@alumnos.upm.es>
2021-06-20 16:59:53 +0100

    Add MGT script for testing

--- HAL/Tests/EquipementHandlers/CommandLineTests/MGT.bash
+++ HAL/Tests/EquipementHandlers/CommandLineTests/MGT.bash
@@ -8,51 +8,53 @@ h2d(){
 }
 
 i2c_address=0x1C
-sensitivity=0.14
+sensitivity=0.58
 
 echo "SOFT reset..."
 i2cset -y 1 $i2c_address 0x21 0x04 b
 sleep 1
 
 echo "Setting Offsets to 0:"
-i2cset -y 1 $i2c_address 0x05 0x00 b
-i2cset -y 1 $i2c_address 0x06 0x00 b
-i2cset -y 1 $i2c_address 0x07 0x00 b
-i2cset -y 1 $i2c_address 0x08 0x00 b
-i2cset -y 1 $i2c_address 0x09 0x00 b
-i2cset -y 1 $i2c_address 0x0A 0x00 b
+i2cget -y 1 $i2c_address 0x05 #0x00 b
+i2cget -y 1 $i2c_address 0x06 #0x00 b
+i2cget -y 1 $i2c_address 0x07 #0x00 b
+i2cget -y 1 $i2c_address 0x08 #0x00 b
+i2cget -y 1 $i2c_address 0x09 #0x00 b
+i2cget -y 1 $i2c_address 0x0A #0x00 b
 
 echo "Checking WHO_AM_I"
 who=`i2cget -y 1 $i2c_address 0x0F b`
-[ $who = 0x3d  ] && echo "  OK" || echo "  NOK"
+[ $who = 0x3d  ] && echo "$who=0x3d  OK" || echo "$who!=0x3d  NOK"
 
 # CTRL_REG_1_M:
-# 1101 0000, ODR: 10 Hz, HPMode for X and Y, temp. compensation.
-i2cset -y 1 $i2c_address 0x20 0xD0 b
+# 1101 1100, ODR: 10 Hz, HPMode for X and Y, temp. compensation.
+i2cset -y -r 1 $i2c_address 0x20 0xDC b
 # CTRL_REG_2_M:
-# 0000 0000, FS: +- 4 gauss.
-i2cset -y 1 $i2c_address 0x21 0x00 b
+# 0110 0000, FS: +- 16 gauss.
+i2cset -y -r 1 $i2c_address 0x21 0x60 b
 # CTRL_REG_3_M:
 # 0000 0000, I2C enabled, continuous-conversion mode
-i2cset -y 1 $i2c_address 0x22 0x00 b
+i2cset -y -r 1 $i2c_address 0x22 0x00 b
 # CTRL_REG_4_M:
 # 0000 1000, Little endian, HPMode for Z
-i2cset -y 1 $i2c_address 0x23 0x08 b
+i2cset -y -r 1 $i2c_address 0x23 0x08 b
 # CTRL_REG_5_M:
 # 0100 0000, NOT Fast Read, Block data update
-i2cset -y 1 $i2c_address 0x24 0x40 b
+i2cset -y -r 1 $i2c_address 0x24 0x40 b
 
 while true
 do
   xb=`i2cget -y 1 $i2c_address 0x28 w`;
   xr=`h2d $xb`
-  x=$( bc -l <<< "$xr*$sensitivity*-1")
+  x=$( bc -l <<< "$xr*$sensitivity")
   yb=`i2cget -y 1 $i2c_address 0x2A w`;
   yr=`h2d $yb`
   y=$( bc -l <<< "$yr*$sensitivity")
   zb=`i2cget -y 1 $i2c_address 0x2C w`;
   zr=`h2d $zb`
   z=$( bc -l <<< "$zr*$sensitivity")
-  echo "[$xb, $yb, $zb] bin    [$x, $y, $z] mgauss"
+  echo "[$xb, $yb, $zb] bin    [$xr, $yr, $zr] raw   [$x, $y, $z] mgauss"
+  echo "$xr, $yr, $zr, $x, $y, $z" &>> ./log_mgt.csv
+
   sleep 0.5
 done
